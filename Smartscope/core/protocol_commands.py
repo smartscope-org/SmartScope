e871ec62a614611e6812bd8100139bd12c1f9bf6
Jonathan Bouvette <jonathan.bouvette@gmail.com>
2026-04-14 10:22:38 -0400

    changed squareInMediumMag to use the correct area value

--- Smartscope/core/protocol_commands.py
+++ Smartscope/core/protocol_commands.py
@@ -65,8 +65,8 @@ def square(scope:MicroscopeInterface,params,instance, content:Dict, *args, **kwa
 def squareInMediumMag(scope:MicroscopeInterface,params,instance, content:Dict, *args, **kwargs)  -> None:
     """Acquires and save the square image using the View preset."""
     size_x, size_y = scope.get_mag_area_in_microns(magSet='V')
-    
-    total_area_size = np.sqrt(instance.area) / 10 * 1.4
+    area = instance.selectors.filter(method_name='Size selector').first().value
+    total_area_size = np.sqrt(area) * 1.3
     logger.debug(f'Medium mag size in A: {size_x} x {size_y}, total area size: {total_area_size}')
     n_tiles_x = int(np.ceil(total_area_size / size_x))
     n_tiles_y = int(np.ceil(total_area_size / size_y))
