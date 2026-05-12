326934c49dc7c7a610ebaec14f2ad2d92dd5187f
Anna Bogomolova <bogomolovaa2@nih.gov>
2026-05-12 15:24:51 -0400

    fix: on square processing error, update selected flag

--- Smartscope/core/grid/run_square.py
+++ Smartscope/core/grid/run_square.py
@@ -93,6 +93,7 @@ class RunSquare:
         except Exception as e:
             square = update(square,
                     status=status.ERROR,
+                    selected=False,
                     completion_time=timezone.now()
                 )
             logger.info(f"Status of the square {square.name} is set to ERROR")
